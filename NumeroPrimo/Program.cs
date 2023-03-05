2462fb59949b178dfac44d235d68fdc38965bf16
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 16:13:04 -0300

    Add project files.

---
+++ NumeroPrimo/Program.cs
@@ -0,0 +1,19 @@
+﻿float num;
+int aux;
+
+Console.WriteLine("Digite um numero para saber se e primo: ");
+num = float.Parse(Console.ReadLine());
+
+aux = 0;
+for (int i = 2; i < num; i++)
+{
+    if (num % i == 0)
+        aux = i;
+}
+
+if (aux == 0)
+    Console.WriteLine("Este numero é primo!");
+else
+{
+    Console.WriteLine("Este numero nao e primo!");
+}
