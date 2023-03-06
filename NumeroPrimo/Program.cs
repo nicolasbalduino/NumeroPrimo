ccd7e9bb70f7b3d59bd65708471ee4ec861fe512
nicolasbalduino <nicolasbalduino@outlook.com>
2023-03-05 21:34:06 -0300

    Criada funcao que testa se o numero informado e primo, e caso positivo, guarda seu ultimo divisor

--- NumeroPrimo/Program.cs
+++ NumeroPrimo/Program.cs
@@ -1,15 +1,18 @@
 ﻿float num;
 int aux;
 
+void NumeroPrimo()
+{
+    for (int i = 2; i < num; i++)
+        if (num % i == 0)
+            aux = i;
+}
+
 Console.WriteLine("Digite um numero para saber se e primo: ");
 num = float.Parse(Console.ReadLine());
 
 aux = 0;
-for (int i = 2; i < num; i++)
-{
-    if (num % i == 0)
-        aux = i;
-}
+NumeroPrimo();
 
 if (aux == 0)
     Console.WriteLine("Este numero é primo!");
